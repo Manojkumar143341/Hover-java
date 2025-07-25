51d5eb7774a2fda5e5c4e3789521d9bc73eb5e78
Manoj kumar <manoj166174@gmail.com>
2025-07-25 23:25:51 +0530

    Add files via upload

---
+++ s.java
@@ -0,0 +1,16 @@
+public class s{
+    String name;
+    String college;
+   void disply(){
+    System.out.println(name);
+   }
+
+
+
+   public static void main(String[] args) {
+      s t1 = new s();
+      t1.name="manoj";
+      t1.college="ciet";
+       t1.disply();
+   }
+}
